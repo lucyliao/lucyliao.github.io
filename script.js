5a87ba8d3cccf249328b0d20f181d7293173e126
Lucy Liao <lucynliao@gmail.com>
2016-08-08 17:13:46 -0700

    Add jQuery animation effects to homepage and illustration page

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-$(document).ready(function (){
-    // highlights active page in nav bar - needs to be fixed
-    var str=location.href.toLowerCase();
-    $(".navigation li a").each(function() {
-        if (str.indexOf(this.href.toLowerCase()) > -1) {
-            $("li.highlight").removeClass("highlight");
-            $(this).parent().addClass("highlight");
-          }
-    });
-    // isotope autofits thumbnails on page
-    $('.grid').isotope({
-        itemSelector: '.grid-item',
-        masonry: {
-            columnWidth: 250
-        }
-    });
-    // magnific popup
-    $('.image-link').magnificPopup({type:'image'});
-    $('.parent-container').magnificPopup({
-        delegate: 'a',
-        type: 'image'
-        gallery:{enabled:true}
-        callbacks: {
-            buildControls: function() {
-        this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
-    }
-}
-    });
-});
-$(document).ready(function () {
-    $('#button').click(function () {
-        $('.circle').fadeToggle(300);
-    });
+$(function(){
+  $('a[href$="#"]').on('click', function(e){
+    e.preventDefault();
+  });
+ 
+  $('#pagehead').delay(400).animate({opacity: '1', top: '0'});
+  $('.thumb-img').delay(1000).animate({opacity: '1'},850);
 });
